97d4c0eb587efe86722e95c0ba5976b5e0222c53
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-07-29 09:28:39 -0400

    Downloaded And installed App CalculatorLayout .

---
+++ app/src/main/java/ca/davidpellegrini/calculatorlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package ca.davidpellegrini.calculatorlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Calculator Layout",Toast.LENGTH_SHORT).show();
+    }
+}
